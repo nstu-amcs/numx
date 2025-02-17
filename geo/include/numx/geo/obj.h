7d49b23086eaafbe864eda59380ca9e1a0690472
Ivan Paraskun <ig.paraskun@gmail.com>
2025-02-17 21:05:57 +0700

    upd: vtx generations.

--- geo/include/numx/geo/obj.h
+++ geo/include/numx/geo/obj.h
@@ -3,14 +3,37 @@
 
 #include <numx/geo/geo.h>
 #include <stdio.h>
+#include <stdx/cut.h>
 
+/// @brief Complex geometric object.
 struct obj {
-  struct vtx** v;
-  struct rct** r;
-  struct hxd** h;
+  /// @brief Object's vertices.
+  struct pcut v;
+
+  /// @brief Object's quadrangles.
+  struct pcut q;
+
+  /// @brief Object's hexahedrons.
+  struct pcut h;
 };
 
 int obj_get(struct obj* o, FILE* f);
+
+int obj_get_vtx(struct vtx* v, FILE* f);
+int obj_get_qud(struct qud* q, FILE* f);
+int obj_get_hxd(struct hxd* h, FILE* f);
+
 int obj_put(struct obj* o, FILE* f);
+
+int obj_put_vtx(struct vtx* v, FILE* f);
+int obj_put_qud(struct qud* q, FILE* f);
+int obj_put_hxd(struct hxd* h, FILE* f);
+
+/** @brief Generate complex continuous object using supplied step function.
+ *
+ *  @param i initial vertex index
+ *  @param s step function
+ */
+int obj_gen_hxd(struct obj* o, int i, struct icap s);
 
 #endif  // NUMX_GEO_OBJ_H
