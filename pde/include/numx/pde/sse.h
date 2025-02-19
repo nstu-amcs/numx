dd84cfec1d00f8b16a9792a40dea2bdeeeaf5d02
Ivan Paraskun <ig.paraskun@gmail.com>
2025-02-19 10:05:04 +0700

    upd: pde layout.

---
+++ pde/include/numx/pde/sse.h
@@ -0,0 +1,9 @@
+#ifndef NUMX_PDE_SSE_H
+#define NUMX_PDE_SSE_H
+
+#include <numx/pde/cnd.h>
+
+int pde_sse_fem_slv();
+int pde_sse_fde_slv();
+
+#endif  // NUMX_PDE_SSE_H
---
+++ pde/include/numx/pde/tde.h
@@ -0,0 +1,8 @@
+#ifndef NUMX_PDE_TDE_H
+#define NUMX_PDE_TDE_H
+
+#include <numx/pde/cnd.h>
+
+int pde_tde_fem_slv();
+
+#endif  // NUMX_PDE_TDE_H
