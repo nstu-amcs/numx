6f206f7cfec7aee79f02b47e8cc11f5fb10950f0
Ivan Paraskun <ig.paraskun@gmail.com>
2025-03-14 20:15:39 +0700

    upd: pde/geo: nonlinear

--- pde/include/numx/pde/sse.h
+++ pde/include/numx/pde/sse.h
@@ -7,52 +7,67 @@
 #include <numx/vec/vec.h>
 #include <stdx/cut.h>
 
-/// @brief Face's context.
-struct fctx {
-  struct cnd cnd;
+/** Face's context. */
+struct fctx
+{
+    struct cnd cnd;
 };
 
-int fctx_get(void* ctx, int n, ...);
+int fctx_get(void *ctx, int n, ...);
 
-/// @brief Element's context.
-struct ectx {
-  double lam;
-  double gam;
-
-  double (*ext)(struct vtx* v);
+/** Finite element's context. */
+struct ectx
+{
+    fun lam;
+    fun gam;
+    fun ext;
 };
 
-int ectx_get(void* ctx, int n, ...);
+int ectx_get(void *ctx, int n, ...);
 
-struct sse_ops {
-  struct {
-    enum iss_mod mod;
-    union {
-      struct iss_jac_ops jac;
-      struct iss_rlx_ops rlx;
-      struct iss_bcg_ops bcg;
-    } ops;
-  } iss;
+/** Options for boundary problem solver. */
+struct sse_ops
+{
+    int    max; // Maximum number of iterations (set 1 for linear problems)
+    double eps;
+
+    struct
+    {
+        enum iss_mod mod;
+        union
+        {
+            struct iss_jac_ops jac;
+            struct iss_rlx_ops rlx;
+            struct iss_bcg_ops bcg;
+        } ops;
+    } iss;
 };
 
-struct sse_fdm_ops {
-  struct sse_ops ops;
+/** Options for finite difference boundary problem solver. */
+struct sse_fdm_ops
+{
+    struct sse_ops ops;
 };
 
-struct sse_fem_ops {
-  enum { FEM_LIN } mod;
+/** Options for finite element boundary problem solver. */
+struct sse_fem_ops
+{
+    struct sse_ops ops;
 
-  struct sse_ops ops;
+    enum fem_mod
+    {
+        FEM_LIN // Trilinear basis
+    } mod;
 };
 
-/// @brief Finite Difference Method boundary problem solver.
+/// @brief Finite Difference boundary problem solver.
 /// @param o domain object
 /// @param x solution vector
-int pde_sse_fdm_slv(struct obj* obj, struct vec* x, struct sse_fdm_ops ops);
+int pde_sse_fdm_slv(struct obj *obj, struct vec *x, struct sse_fdm_ops ops);
 
-/// @brief Finite Element Method boundary problem solver.
+/// @brief Finite Element boundary problem solver.
 /// @param o domain object
 /// @param x solution vector
-int pde_sse_fem_slv(struct obj* obj, struct vec* x, struct sse_fem_ops ops);
+int pde_sse_fem_slv(struct obj *obj, struct vec *x, struct sse_fem_ops ops);
 
-#endif  // NUMX_PDE_SSE_H
+#endif // NUMX_PDE_SSE_H
